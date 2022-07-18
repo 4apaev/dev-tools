4bb19331eff107206f38dfd826bf21817ed6ac58
michael <4apaev@gmail.com>
2022-07-18 03:53:10 +0300

    fix failed test

--- test/object.spec.js
+++ test/object.spec.js
@@ -1,22 +1,18 @@
 import test from 'node:test'
-import {
-  strictEqual as eq,
-  deepStrictEqual as equal,
-} from 'assert'
-
+import { deepStrictEqual as equal } from 'assert'
 import { pick, omit } from '../src/object.js'
 
-test('Pick', t => {
+test('Pick', _ => {
   const o = { a: 1, b: 2, c: 3, d: 4 }
   equal(pick(o, 'a', 'c'), { a: 1, c: 3 }, 'should pick "a", "c" fields')
   equal(pick(o, 'b', 'd'), { b: 2, d: 4 }, 'should pick "b", "d" fields')
   console.log('Pick')
 })
 
-test('Omit', t => {
+test('Omit', _ => {
   const o = { a: 1, b: 2, c: 3, d: 4 }
   equal(omit(o, 'a', 'c'), { b: 2, d: 4 }, 'should omit "a", "c" fields')
-  equal(omit(o, 'b', 'd'), { w: 1, c: 3 }, 'should omit "b", "d" fields')
+  equal(omit(o, 'b', 'd'), { a: 1, c: 3 }, 'should omit "b", "d" fields')
   console.log('Omit')
 })
 
